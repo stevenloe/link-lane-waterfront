ae453650a4ad78ac233d29b7f80f657a7949bce4
Steven Loe <stevenjloe@protonmail.com>
2022-05-20 14:44:26 -0400

    initial commit

---
+++ components/IndexComp.vue
@@ -0,0 +1,135 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <header
+  class="  h-screen mb-12 bg-fixed bg-center bg-cover custom-img"
+>
+<Navbar/>
+  <div class="p-5 text-2xl text-white bg-purple-300 bg-opacity-50 rounded-xl">
+    <div class="h-screen flex  flex-wrap items-center -mt-24">
+      <div class="w-3/5">
+        
+              <h1 class="font-extrabold text-white text-6xl w-full mb-8">Cozy Cottage in the Heart of SOMA</h1>
+              <h2 class="text-white font-semibold mb-4">500 Terry Francois St. San Francisco, CA</h2>
+              <h2 class="text-white font-semibold mb-24">•  1 Living Room  •  2 Bedrooms  •  2 Bathrooms  •  1 Garage</h2>
+              <h2 class="text-white font-black text-6xl mb-4">$2,300,000</h2>
+
+      </div>
+      <div>
+        <svg preserveAspectRatio="none" data-bbox="20.297 58.962 159.407 82.076" viewBox="20.297 58.962 159.407 82.076" height="200" width="200" xmlns="http://www.w3.org/2000/svg" data-type="shape" role="presentation" aria-hidden="true">
+    <g>
+        <path d="M176.082 59.629l-76.326 76.047-75.768-75.769c-.836-.836-2.228-.836-3.064 0s-.836 2.228 0 3.064l77.44 77.44a2.185 2.185 0 0 0 3.064 0v-.279h.279l77.44-77.44c.557-.557.557-1.114.557-1.671 0-.557-.279-1.114-.557-1.671-.836-.557-2.229-.557-3.065.279z"></path>
+    </g>
+</svg>
+      </div>
+    </div>
+  </div>
+</header>
+<div class="max-w-lg m-auto">
+  <p class="mb-4">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat a
+    magna non varius. Proin leo felis, euismod non porta eget, varius sit amet
+    sapien. Maecenas in nulla at leo convallis consectetur id a sapien. Nulla
+    nec pulvinar nisi. Vivamus non facilisis lacus, et volutpat libero. Nulla ac
+    odio aliquam, accumsan arcu ut, lacinia est. Nulla eu sem elit. Fusce nec
+    laoreet sem, semper molestie libero.
+  </p>
+  <p class="mb-4">
+    Ut sagittis lacus consequat accumsan venenatis. Sed sollicitudin, lectus et
+    fringilla ultrices, dolor nisi scelerisque tortor, vel finibus magna massa
+    non nunc. Phasellus massa quam, egestas a nisl sed, porta volutpat metus.
+    Nunc sed elit ac tellus tempor cursus. Suspendisse potenti. Vestibulum
+    varius rutrum nisl nec consequat. Suspendisse semper dignissim sem viverra
+    semper. Nulla porttitor, purus nec accumsan pharetra, nisi dolor condimentum
+    ipsum, id consequat nulla nunc in ligula.
+  </p>
+  <p class="mb-12">
+    Nulla pharetra lacinia nisi, vitae mollis tellus euismod id. Mauris porta
+    dignissim hendrerit. Cras id velit varius, fermentum lectus vitae, ultricies
+    dolor. In bibendum rhoncus purus vel rutrum. Nam vulputate imperdiet
+    fringilla. Donec blandit libero massa. Suspendisse dictum diam mauris, vitae
+    fermentum dolor tincidunt in. Pellentesque sollicitudin venenatis dolor,
+    vitae scelerisque elit ultrices eu. Donec eget sodales risus, quis dignissim
+    neque.
+  </p>
+</div>
+<section
+  class="container flex items-center justify-center h-screen m-auto mb-12 bg-fixed bg-center bg-cover custom-img"
+>
+  <div class="p-5 text-2xl text-white bg-purple-300 bg-opacity-50 rounded-xl">
+    Parralax inline
+  </div>
+</section>
+<div class="max-w-lg m-auto">
+  <p class="mb-4">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat a
+    magna non varius. Proin leo felis, euismod non porta eget, varius sit amet
+    sapien. Maecenas in nulla at leo convallis consectetur id a sapien. Nulla
+    nec pulvinar nisi. Vivamus non facilisis lacus, et volutpat libero. Nulla ac
+    odio aliquam, accumsan arcu ut, lacinia est. Nulla eu sem elit. Fusce nec
+    laoreet sem, semper molestie libero.
+  </p>
+  <p class="mb-4">
+    Ut sagittis lacus consequat accumsan venenatis. Sed sollicitudin, lectus et
+    fringilla ultrices, dolor nisi scelerisque tortor, vel finibus magna massa
+    non nunc. Phasellus massa quam, egestas a nisl sed, porta volutpat metus.
+    Nunc sed elit ac tellus tempor cursus. Suspendisse potenti. Vestibulum
+    varius rutrum nisl nec consequat. Suspendisse semper dignissim sem viverra
+    semper. Nulla porttitor, purus nec accumsan pharetra, nisi dolor condimentum
+    ipsum, id consequat nulla nunc in ligula.
+  </p>
+  <p class="mb-4">
+    Nulla pharetra lacinia nisi, vitae mollis tellus euismod id. Mauris porta
+    dignissim hendrerit. Cras id velit varius, fermentum lectus vitae, ultricies
+    dolor. In bibendum rhoncus purus vel rutrum. Nam vulputate imperdiet
+    fringilla. Donec blandit libero massa. Suspendisse dictum diam mauris, vitae
+    fermentum dolor tincidunt in. Pellentesque sollicitudin venenatis dolor,
+    vitae scelerisque elit ultrices eu. Donec eget sodales risus, quis dignissim
+    neque.
+  </p>
+</div>
+<div id="info" class="max-w-lg m-auto">
+  <h2 class="mb-4 text-6xl font-bold">
+    This is the info section
+  </h2>
+  <p class="mb-4">
+    INFO Ut sagittis lacus consequat accumsan venenatis. Sed sollicitudin, lectus et
+    fringilla ultrices, dolor nisi scelerisque tortor, vel finibus magna massa
+    non nunc. Phasellus massa quam, egestas a nisl sed, porta volutpat metus.
+    Nunc sed elit ac tellus tempor cursus. Suspendisse potenti. Vestibulum
+    varius rutrum nisl nec consequat. Suspendisse semper dignissim sem viverra
+    semper. Nulla porttitor, purus nec accumsan pharetra, nisi dolor condimentum
+    ipsum, id consequat nulla nunc in ligula.
+  </p>
+  <p class="mb-4">
+    MORE INFO Nulla pharetra lacinia nisi, vitae mollis tellus euismod id. Mauris porta
+    dignissim hendrerit. Cras id velit varius, fermentum lectus vitae, ultricies
+    dolor. In bibendum rhoncus purus vel rutrum. Nam vulputate imperdiet
+    fringilla. Donec blandit libero massa. Suspendisse dictum diam mauris, vitae
+    fermentum dolor tincidunt in. Pellentesque sollicitudin venenatis dolor,
+    vitae scelerisque elit ultrices eu. Donec eget sodales risus, quis dignissim
+    neque.
+  </p>
+</div>
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar.vue';
+export default {
+    name: "NuxtTutorial",
+    components: { Navbar }
+}
+</script>
+
+<style>
+html {
+	scroll-behavior: smooth;
+}
+
+.custom-img {
+  background-image: url("/img/sample.jpg");
+}
+
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <index-comp></index-comp>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
